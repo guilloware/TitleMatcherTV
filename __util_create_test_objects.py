103ed5436277a6050731e5d3571aa32a565ed9d2
Johnny Driver <12505394+guilloware@users.noreply.github.com>
2022-08-09 23:37:01 +1000

    api call implements another search method create test utils better structure

--- __util_create_test_objects.py
+++ __util_create_test_objects.py
@@ -1,39 +1,50 @@
 import os
 import glob
+from pathlib import Path
+
+def delete(parent_dir):
+    files = glob.glob(f"{parent_dir}*")
+    for f in files:
+        isDirectory = os.path.isdir(f)
+        if isDirectory:
+            os.rmdir(f)
+        else:
+            os.remove(f)
 
 # Parent directory
 parent_dir = ".\\__fixtures__\\"
 
 # Remove Directories
-files = glob.glob(f"{parent_dir}*")
-for f in files:
-    isDirectory = os.path.isdir(f)
-    if isDirectory:
-        os.rmdir(f)
-    else:
-        os.remove(f)
+delete(parent_dir)
+
+
 
 # Create Test Dir
-directory = "The Simpsons"
-path = os.path.join(parent_dir, directory)
+series_directory_name = "The Simpsons"
+series_path = os.path.join(parent_dir, series_directory_name)
+print(series_path)
+os.mkdir(series_path)
+
+# Create Test Dir
+season_directory_04 = "Season 04"
+path = os.path.join(series_path, season_directory_04)
+os.mkdir(path)
+
+# Create Test Dir
+season_directory_05 = "The Simpsons - Season 5"
+path = os.path.join(series_path, season_directory_05)
+os.mkdir(path)
+
+media_file_name_5 = "The Simpsons [5x01] Homer's Barbershop Quartet.avi"
+media_path = os.path.join(series_path, season_directory_05, media_file_name_5)
+f = open(media_path, "x")
+
+# Create Test Dir
+season_directory_01 = "Simpson 1"
+path = os.path.join(series_path, season_directory_01)
 os.mkdir(path)
 
 # Create Test Media
-media = "A Streetcar Named Marge.mp4"
-mediaPath = os.path.join(parent_dir, media)
-f = open(mediaPath, "x")
-
-# Create Test Dir
-directory = "Season 02"
-path = os.path.join(parent_dir, directory)
-os.mkdir(path)
-
-# Create Test Dir
-directory = "The Simpsons - Season 6"
-path = os.path.join(parent_dir, directory)
-os.mkdir(path)
-
-# Create Test Dir
-directory = "Simpson 1"
-path = os.path.join(parent_dir, directory)
-os.mkdir(path)
+media_file_name = "A Streetcar Named Marge.mp4"
+media_path = os.path.join(series_path, season_directory_04, media_file_name)
+f = open(media_path, "x")
--- api.py
+++ api.py
@@ -9,9 +9,19 @@ def search(url):
 	results = response.json()["results"]
 	return results
 
-def searchEpisode(text):
-	url = f"https://imdb-api.com/en/API/SearchEpisode/{api_key}/{text}"
-	return search(url)
+def searchEpisode(file_name):
+	search_methods = []
+	search_methods.append(file_name)
+	try: 
+		search_methods.append(file_name.split(']')[1])
+	except:
+		print("-search method warning-")
+
+	for text in search_methods:
+		url = f"https://imdb-api.com/en/API/SearchEpisode/{api_key}/{text}"
+		results =  search(url)
+		if len(results) > 0:
+			return results		
 
 def searchSeries(text):
 	url = f"https://imdb-api.com/en/API/SearchSeries/{api_key}/{text}"
